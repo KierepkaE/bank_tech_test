04d50a710a8ed9d1af0c17673daf271145a35388
kierepkae <kierepkaedyta@gmail.com>
2019-09-26 14:13:09 +0100

    fix empty lines

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,9 +1,7 @@
 # frozen_string_literal: true
+
 require 'simplecov'
 require 'simplecov-console'
-
-
-
 
 SimpleCov.formatter = SimpleCov::Formatter::MultiFormatter[
   SimpleCov::Formatter::HTMLFormatter,
@@ -55,7 +53,6 @@ RSpec.configure do |config|
   # inherited by the metadata hash of host groups and examples, rather than
   # triggering implicit auto-inclusion in groups with matching metadata.
   config.shared_context_metadata_behavior = :apply_to_host_groups
-
 
   # The settings below are suggested to provide a good initial experience
   # with RSpec, but feel free to customize to your heart's content.
