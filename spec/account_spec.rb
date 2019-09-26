d69148c84e6cf68d2797a48638c91f01c267eda4
kierepkae <kierepkaedyta@gmail.com>
2019-09-26 09:22:48 +0100

    red test for putting money in the account

--- spec/account_spec.rb
+++ spec/account_spec.rb
@@ -1,10 +1,18 @@
 require 'account'
+require 'date'
 
 
 describe Account do
   it 'has default balance of 0' do
     account = Account.new
     expect(account.balance).to eq 0
+  end
+
+  it 'can be given a deposit' do
+    account = Account.new
+    date = Date.new(2019,9,26)
+    account.add(100,date)
+    expect(account.balance).to eq 100
   end
 end
 
