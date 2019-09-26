be182f7acc83736000c2a799fa54b39ae2645755
kierepkae <kierepkaedyta@gmail.com>
2019-09-26 09:43:10 +0100

    red test for account history

--- spec/account_spec.rb
+++ spec/account_spec.rb
@@ -14,6 +14,21 @@ describe Account do
     account.add(100,date)
     expect(account.balance).to eq 100
   end
+
+  it 'have access to account history ' do
+    account = Account.new
+    date = Date.new(2019,9,26)
+    account.add(500,date)
+    expect(account.balance).to eq 500
+    expect(account.history.show).should include(
+     {
+      date: date,
+      debit: false,
+      amount: '500.00',
+      balance: '500.00'
+     }
+    )
+  end
 end
 
 
